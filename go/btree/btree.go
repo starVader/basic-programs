ea43def25bc17bdbc96465f852826ae258ee1e44
Rahul Kumar <84134196+commitRahul@users.noreply.github.com>
2022-04-21 23:32:01 +0530

    Update btree
    1) Fixed insertion, since new node was not getting assigned
    2) Removed "fmt.Println("in traverse", t.data)" line, since it was giving seg fault in case of t = nil

--- go/btree/btree.go
+++ go/btree/btree.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
-	)
+)
 
-type tree struct{
-	left,right *tree
-	data int
+type tree struct {
+	left, right *tree
+	data        int
 }
 
 //inorder traversal
-func traverse(t *tree){   // passing the root and channel
-	fmt.Println("in traverse",t.data)
+func traverse(t *tree) { // passing the root and channel
+	// fmt.Println("in traverse", t.data)
 	if t == nil {
 		return
-	}else {
+	} else {
 		traverse(t.left)
 		fmt.Println(t.data)
 		traverse(t.right)
@@ -22,30 +22,29 @@ func traverse(t *tree){   // passing the root and channel
 }
 
 func insert(t *tree, l *tree) *tree {
-	if t == nil{
+	if t == nil {
+		t = l
 		return t
 	}
 	if l.data < t.data {
 		fmt.Println("Inserting into left")
-		insert(t.left,l)
-	}else{
+		t.left = insert(t.left, l)
+	} else {
 		fmt.Println("Inserting into right")
-		insert(t.right,l)
+		t.right = insert(t.right, l)
 	}
 	return t
 }
 
-
 func main() {
-	var  t = new(tree)
+	var t = new(tree)
 	k := 80
 	t.data = k
-	var  newnode = new(tree)
+	var newnode = new(tree)
 	newnode.data = 20
-	insert(t,newnode)
-	newnode.data = 90
-	insert(t,newnode)
+	t = insert(t, newnode)
+	var newnode1 = new(tree)
+	newnode1.data = 90
+	t = insert(t, newnode1)
 	traverse(t)
-
 }
-
